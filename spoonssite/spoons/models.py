ac885a9a949809ddb7fa5c589d73bc48fb9c46ba
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-11-28 18:54:57 -0500

    User object and Task object functional No UI yet.

--- spoonssite/spoons/models.py
+++ spoonssite/spoons/models.py
@@ -2,21 +2,30 @@ from django.db import models
 
 class User(models.Model):
     user_key = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=20)
+    user_name = models.ForeignKey('auth.User')
     join_date = models.DateTimeField('Date Joined')
     birthdate = models.DateTimeField('Birthdate')
     average_spoons = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.user_key
+        return('%s' % self.user_name)
 
-    def create_user(self):
-        pass
 
 class Task(models.Model):
+    TASK_CHOICES = (
+        ('SHOWER', 'Shower'),
+        ('BRUSH TEETH', 'Brush Teeth'),
+        ('DO HOMEWORK', 'Do Homework'),
+        ('BRUSH HAIR', 'Brush Hair'),
+        ('GET DRESSED', 'Get Dressed'),
+    )
+
     task_key = models.AutoField(primary_key=True)
-    task = models.CharField(max_length=60)
+    task = models.CharField(max_length=20,
+        choices=TASK_CHOICES,
+        default = 'SHOWER'
+    )
     value = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.task_key
+        return self.task
