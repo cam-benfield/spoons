724c22879c1584882676d35c70fa5fd5a7e4207e
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-11-28 20:00:13 -0500

    Edited Settings for PythonAnywhere Setup

--- spoonssite/spoonssite/settings.py
+++ spoonssite/spoonssite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'my0@5u88zvdae$(n&+po8zoi9*&agf6hjow^++p2od$*jdody-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
