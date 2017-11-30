8e7b61f69796dfc2bc2cd763b6d111b3b5f1578c
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2017-11-29 22:49:29 -0500

    Debugging Queries

--- spoonssite/spoons/urls.py
+++ spoonssite/spoons/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index', views.index, name='index'),
     url(r'^task_list', views.task_list, name='tasks'),
-    url(r'^task/(?P<pk>\d+)/$', views.task_detail, name='task_detail'),
     url(r'^user_list', views.user_list, name='users'),
+    url(r'^task/(?P<pk>\d+)/$', views.task_detail, name='task_detail'),
     url(r'^user/(?P<pk>\d+)/$', views.user_detail, name='user_detail'),
     url(r'^task/new/$', views.task_new, name='task_new'),
     url(r'^user/new/$', views.user_new, name='user_new'),
+    url(r'^user_query', views.user_query, name = 'user_query'),
+    url(r'^task_query', views.task_query, name = 'task_query'),
 ]
--- spoonssite/spoons/views.py
+++ spoonssite/spoons/views.py
@@ -7,6 +7,7 @@ from .forms import TaskForm, UserForm
 def index(request):
     return render(request, 'spoons/index.html')
 
+# Task Views
 def task_list(request):
     tasks = SpoonTask.objects.all() # NOTE: Make User Dynamic
     return render(request, 'spoons/task_list.html', {'tasks': tasks})
@@ -14,14 +15,6 @@ def task_list(request):
 def task_detail(request, pk):
     task = SpoonTask.objects.get(pk=pk)
     return render(request, 'spoons/task_detail.html', {'task': task})
-
-def user_list(request):
-    users = SpoonUser.objects.all() # NOTE: Make User more Dynamic
-    return render(request, 'spoons/user_list.html', {'users': users})
-
-def user_detail(request, pk):
-    user = SpoonUser.objects.get(pk=pk)
-    return render(request, 'spoons/user_detail.html', {'user': user})
 
 def task_new(request):
     if request.method == "POST":
@@ -34,6 +27,20 @@ def task_new(request):
         form = TaskForm()
     return render(request, 'spoons/task_new.html', {'form': form})
 
+def task_query(request, user):
+    us = str(auth.User)
+    tasks = SpoonTask.objects.get(task_user=us)
+    return render(request, 'task_query.html', {'tasks': tasks})
+
+# User Views
+def user_list(request):
+    users = SpoonUser.objects.all() # NOTE: Make User more Dynamic
+    return render(request, 'spoons/user_list.html', {'users': users})
+
+def user_detail(request, pk):
+    user = SpoonUser.objects.get(pk=pk)
+    return render(request, 'spoons/user_detail.html', {'user': user})
+
 def user_new(request):
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -44,3 +51,7 @@ def user_new(request):
     else:
         form = UserForm()
     return render(request, 'spoons/user_new.html', {'form': form})
+
+def user_query(request, tk): # finds users who have the task
+    users = SpoonTask.objects.filter(tk)
+    return render(request, '/user_query.html', {'users': users})
